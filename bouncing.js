2beaffb0cb5904a6b11204ff2bcb46c97b66d8c1
marie-phu-qui <marie.nguyen.phu.qui@gmail.com>
2018-07-12 10:10:48 +1200

    update js

--- bouncing.js
+++ bouncing.js
@@ -1,24 +1,16 @@
 // change element size according to length of click
 
-//make element appear on click
-$(document).ready(function(){
-    $(document).click(function(e){
-        $("body").append(" <div class='square'><p>square</p></div> ");
-        $("div").last().offset({left:e.pageX,top:e.pageY});
-        moveTowardsEdge();
-    });
-});
+
 
 // make element move toward screen edge
 // square object distance to edge top/right/botom/left - choose the smallest one then move towards it
 
 //get window user size and use in here according to where the square is
 
-function moveTowardsEdge(e) {
-};
 
 function moveTop(){
-    $("div").style.bottom = parseInt("div".style.bottom) + 10 + 'px';
+    let square = document.getElementsByClassName("square");
+    square.style.bottom = parseInt(square.style.bottom) + 10 + "px";
     moveTowardsEdge = setTimeout(moveTop,20);
 };
 
@@ -36,3 +28,18 @@ function moveLeft(){
     $("div").style.right = parseInt("div".style.right) + 10 + 'px';
     moveTowardsEdge = setTimeout(moveLeft,20);
 };
+
+function moveTowardsEdge() {
+    let square = document.getElementsByClassName("square");
+    moveTop();
+};
+
+
+//JQUERY make element appear on click
+$(document).ready(function(){
+    $(document).click(function(e){
+        $("body").append(" <div class='square'><p>square</p></div> ");
+        $("div").last().offset({left:e.pageX, right:e.pageX, top:e.pageY, bottom:e.pageY});
+        moveTowardsEdge();
+    });
+});
