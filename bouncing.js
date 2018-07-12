761cfb220c7f0826efc35a1c3f93e839927c5d37
marie-phu-qui <marie.nguyen.phu.qui@gmail.com>
2018-07-13 11:01:18 +1200

    creation of screenWidth and screenHeight to make element move accordingly-not moving yet

--- bouncing.js
+++ bouncing.js
@@ -1,47 +1,29 @@
 // change element size according to length of click
 let square = document.getElementsByClassName("square");
 let a = 10;
+let screenWidth = $(window).width();
+let screenHeight = $(window).height();
 // make element move toward screen edge
+
 // square object distance to edge top/right/botom/left - choose the smallest one then move towards it
 
 //get window user size and use in here according to where the square is
-
-function moveTop(a){
-    $(".square").css("left", a);
-};
-
-function moveRight(){
-    $("div").style.left = parseInt("div".style.left) + 10 + 'px';
-    moveTowardsEdge = setTimeout(moveRight,20);
-};
-
-function moveBottom(){
-    $("div").style.top = parseInt("div".style.top) + 10 + 'px';
-    moveTowardsEdge = setTimeout(moveBottom,20);
-};
-
-function moveLeft(){
-    $("div").style.right = parseInt("div".style.right) + 10 + 'px';
-    moveTowardsEdge = setTimeout(moveLeft,20);
-};
-
-function moveTowardsEdge() {
-    moveTop();
-};
 
 
 //JQUERY make element appear on click
 $(document).ready(function(){
     $(document).click(function createSquare(e){
         $("body").append(" <div class='square'><p>square</p></div> "); 
-        $(".square").last().offset({left:e.pageX, right:e.pageX, top:e.pageY, bottom:e.pageY});
+        $(".square").last().offset({left:e.pageX, top:e.pageY});
         a = e.pageX;
     });
-    if ($(".square")){
+    //make element move towards right if it is closer to the right edge
+    // 
+    if ( parseFloat($(".square:last").css("left")) > (screenWidth/2) ) {
         setInterval(function(){ 
             a +=10;
-            $(".square:last").css("left", a);
-        }, 1000);
-        //$(".square").each(function(index) {
-        //});
-}});
+            $( ".square:last" ).css("left", a);
+        }, 1000);   
+    };
+
+});
