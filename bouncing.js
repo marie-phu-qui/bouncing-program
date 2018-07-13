da90ad7285666ed56c0d235d095f495cc45cdcfe
marie-phu-qui <marie.nguyen.phu.qui@gmail.com>
2018-07-13 16:15:25 +1200

    moved setInterval function in moving squares. very buggy but moving

--- bouncing.js
+++ bouncing.js
@@ -13,16 +13,26 @@ $(document).ready(function(){
         $("body").append(" <div class='square'><p>square</p></div> "); 
         $(".square").last().offset({left:e.pageX, top:e.pageY});
         a = e.pageX;
+        b = e.pageY;
     });
     // square object distance to edge top/right/botom/left - choose the smallest one
 
     //make element move towards right if it is closer to the right edge
     // 
-    if ( parseFloat($(".square:last").css("left")) > (screenWidth/2) ) {
-        setInterval(function(){ 
-            a +=10;
-            $( ".square:last" ).css("left", a);
-        }, 1000);   
-    };
-
+    setInterval(function move(){
+        $(".square").each(function(){
+            if(parseFloat($(".square:last").css("left")) > (screenWidth/2)){
+                setInterval(function goRight(){ 
+                    a +=10;
+                    $( ".square:last" ).css("left", a);
+                }, 1000);   
+            }
+            else if(parseFloat($(".square:last").css("left")) < (screenWidth/2)) {
+                setInterval(function goLeft(){ 
+                    a -=10;
+                    $( ".square:last" ).css("left", a);
+                }, 1000);   
+                };
+            });
+        }, 1000);
 });
