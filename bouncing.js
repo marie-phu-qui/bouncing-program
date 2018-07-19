500a9777149c460027d2cf218895b9870774910b
Your Name <you@example.com>
2018-07-19 09:46:57 +0200

    bug correction

--- bouncing.js
+++ bouncing.js
@@ -7,16 +7,19 @@ let squareSpeedX = 5;
 let squareSpeedY = 5;
 
 // PROBLEME squareHeight and squareWidth return - NaN but parseFloat($(".square").css("height")) returns 50...
-let squareHeight = parseFloat($(".square").css("height"));
-let squareWidth = parseInt($(".square").css("width"));
+let squareHeight;
+let squareWidth;
 
 const framePerSecond = 30;
 
 
 $(document).ready(function(){
   $(document).click(function createSquare(e){
-    $("body").append("<div class='square'<p>square</p></div>");
+    $("body").append('<div class="square"><p>square</p></div>');
     $(".square").last().offset({left:e.pageX, top:e.pageY});
+
+    squareHeight = parseFloat($(".square").eq(0).css("height"));
+    squareWidth = parseInt($(".square").eq(0).css("width"));
 
     squareX = e.pageX;
     squareY = e.pageY;
@@ -28,29 +31,36 @@ $(document).ready(function(){
 
 setInterval(function makeMove(){
   // check is there are squares on the screen
-  if($(".square")){
+  if($(".square").length > 0)
+  {
     // for all the squares do this
     $(".square").each(function(){
+
+      let squareX = $(this).position().left;
+      let squareY = $(this).position().top;
+      // let squareSpeedX = 5;
+      // let squareSpeedY = 5;
+
       squareX += squareSpeedX;
       squareY += squareSpeedY;
-      
+
       // if touch right
-      if(squareX >= screenWidth - squareWidth){
+      if(squareX >= (screenWidth - squareWidth) ){
           squareSpeedX = -squareSpeedX;
-          }   
-      // if touch left     
+          }
+      // if touch left
       else if(squareX <= 0){
-          squareSpeedX = -squareSpeedX;      
+          squareSpeedX = -squareSpeedX;
         }
       // if touch top
-      if(squareY >= screenHeight - squareHeight){
+      if(squareY >= ( screenHeight - squareHeight) ){
           squareSpeedY = -squareSpeedY;
       }
       // if touch bottom
       else if(squareY <= 0){
           squareSpeedY = -squareSpeedY;
       }
-    $(".square").offset({left:squareX, top:squareY})
+    $(this).offset({left:squareX, top:squareY})
     })
   };
 },1000/framePerSecond);
