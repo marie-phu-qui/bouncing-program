1817346cfcd35165afd4a1ed0ffaaf7bf4fdf214
marie-phu-qui <marie.nguyen.phu.qui@gmail.com>
2018-07-23 15:22:17 +1200

    created array for each square speed data - not independant - weird speed and direction

--- bouncing.js
+++ bouncing.js
@@ -1,109 +1,73 @@
 let screenWidth = $(window).width();
 let screenHeight = $(window).height();
 let square = $(".square");
-<<<<<<< HEAD
+let squareNum = $("div.square").length;
 let squareX;
 let squareY;
-let squareSpeedX = 5;
-let squareSpeedY = 5;
+let squareSpeedX = [5];
+let squareSpeedY = [5];
 
 // PROBLEME squareHeight and squareWidth return - NaN but parseFloat($(".square").css("height")) returns 50...
 let squareHeight;
 let squareWidth;
-=======
-let squareX = 0;
-let squareY = 0;
-let squareSpeedX = 1;
-let squareSpeedY = 1;
->>>>>>> bcb9c5d3050ea1a0376f1084d7e970691f43fea3
 
 const framePerSecond = 30;
 
 
 $(document).ready(function(){
   $(document).click(function createSquare(e){
-<<<<<<< HEAD
     $("body").append('<div class="square"><p>square</p></div>');
     $(".square").last().offset({left:e.pageX - 25, top:e.pageY - 25});
 
     squareHeight = parseFloat($(".square").eq(0).css("height"));
     squareWidth = parseInt($(".square").eq(0).css("width"));
 
+    squareSpeedX.push(5);
+    squareSpeedY.push(5);
+
     squareX = e.pageX;
     squareY = e.pageY;
-=======
-    $("body").append("<div class='square'<p>square</p></div>");
-    $(".square").last().offset({left:e.pageX, top:e.pageY});
-
-    e.pageX = squareX;
-    e.pageY = squareY;
->>>>>>> bcb9c5d3050ea1a0376f1084d7e970691f43fea3
 
     screenWidth = $(window).width();
     screenHeight = $(window).height();
   });
 });
 
-setInterval(function makeMove(){
-  // check is there are squares on the screen
-<<<<<<< HEAD
-  if($(".square").length > 0)
-  {
-    // for all the squares do this
-    $(".square").each(function(){
+// check is there are squares on the screen
+//if($("div.square").length>0){
+  setInterval(function makeMove(){
+    squareNum = $("div.square").length;
+    square = $(".square");
+    // check is there are squares on the screen inside the Interval (cost more)
+      for(let i=0; i<squareNum;i++){
+        $(".square").each(function(){
 
-      squareX = $(this).position().left;
-      squareY = $(this).position().top;
+            squareX = $(this).position().left;
+            squareY = $(this).position().top;
 
-      squareX += squareSpeedX;
-      squareY += squareSpeedY;
 
-      // if touch right
-      if(squareX >= (screenWidth - squareWidth) ){
-        squareSpeedX = -squareSpeedX;
-          }
-      // if touch left
-      else if(squareX <= 0){
-        squareSpeedX = -squareSpeedX;
-        }
-      // if touch top
-      else if(squareY >= ( screenHeight - squareHeight) ){
-        squareSpeedY = -squareSpeedY;
-      }
-      // if touch bottom
-      else if(squareY <= 0){
-        squareSpeedY = -squareSpeedY;
-      }
-      $(this).offset({left:squareX, top:squareY})
-    })
-  };
-},1000/framePerSecond);
-=======
-  if($(".square")){
-    // for all the squares do this
-    $(".square").each(function(){
-      squareX += squareSpeedX;
-      squareY += squareSpeedY;
-      
-      // if touch right
-      if(squareX>=screenWidth){
-          squareSpeedX = -squareSpeedX;
-          }   
-      // if touch left     
-      else if(squareX<=0){
-          squareSpeedX = -squareSpeedX;      
-        }
-      // if touch top
-      if(squareY>=screenHeight){
-          squareSpeedY = -squareSpeedY;
-      }
-      // if touch bottom
-      else if(squareY<=0){
-          squareSpeedY = -squareSpeedY;
-      }
-    $(".square").offset({left:squareX, top:squareY})
-    })
-  };
-},1000/framePerSecond);
+            // if touch right
+            if(squareX >= (screenWidth - squareWidth) ){
+              squareSpeedX[i] = -squareSpeedX[i];
+                }
+            // if touch left
+            else if(squareX <= 0){
+              squareSpeedX[i] = -squareSpeedX[i];
+              }
+            // if touch top
+            else if(squareY >= (screenHeight - squareHeight) ){
+              squareSpeedY[i] = -squareSpeedY[i];
+            }
+            // if touch bottom
+            else if(squareY <= 0){
+              squareSpeedY[i] = -squareSpeedY[i];
+            }
 
->>>>>>> bcb9c5d3050ea1a0376f1084d7e970691f43fea3
+            squareX += squareSpeedX[i];
+            squareY += squareSpeedY[i];
+
+            $(this).offset({left:squareX, top:squareY})
+          })
+      }
+    }, 1000/framePerSecond);
+//}
