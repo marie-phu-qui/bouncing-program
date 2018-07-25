a185e78d19cf24f949b26d2bcb1cd16c14938ee1
marie-phu-qui <marie.nguyen.phu.qui@gmail.com>
2018-07-25 12:40:13 +1200

    divided the function to have a better understanding of process- check square with setInterval - move square with for loop

--- bouncing.js
+++ bouncing.js
@@ -1,7 +1,7 @@
 let screenWidth = $(window).width();
 let screenHeight = $(window).height();
 let square = $(".square");
-let squareNum = $("div.square").length;
+let squareNum = $(".square").length;
 let squareX;
 let squareY;
 let squareSpeedX = [5];
@@ -10,6 +10,8 @@ let i;
 
 let squareHeight;
 let squareWidth;
+var squareColors = ["green","blue", "pink"];
+
 
 const framePerSecond = 60;
 
@@ -17,7 +19,7 @@ const framePerSecond = 60;
 $(document).ready(function(){
   $(document).click(function createSquare(e){
     $("body").append('<div class="square"><p>square</p></div>');
-    $(".square").last().offset({left:e.pageX - 25, top:e.pageY - 25});
+    $(".square").last().offset({left:e.pageX - 25, top:e.pageY - 25}).css("background-color", squareColors[Math.floor(Math.random() * squareColors.length)]);
 
     squareHeight = parseFloat($(".square").eq(0).css("height"));
     squareWidth = parseInt($(".square").eq(0).css("width"));
@@ -33,42 +35,52 @@ $(document).ready(function(){
   });
 });
 
-// check is there are squares on the screen
-//if($("div.square").length>0){
-  setInterval(function makeMove(){
-    squareNum = $("div.square").length;
+// check is there are squares on the screen - need to check regulary
+setInterval(function checkSquare(){
+  if($("div.square").length>0){
+    squareNum = $(".square").length;
     square = $(".square");
-    // check is there are squares on the screen inside the Interval (cost more)
 
-        $(".square").each(function(){
-          for(i=0; i<squareNum;i++){
-            squareX = $(this).position().left;
-            squareY = $(this).position().top;
+    //log the presence of square
+    console.log("there is a square on screen")
+  }
+ } , 100000/framePerSecond);
 
-            // if touch right
-            if(squareX >= (screenWidth - squareWidth) ){
-              squareSpeedX[i] = -squareSpeedX[i];
-              }
-            // if touch left
-            else if(squareX <= 0){
-              squareSpeedX[i] = -squareSpeedX[i];
-              }
-            // if touch top
-            else if(squareY >= (screenHeight - squareHeight) ){
-              squareSpeedY[i] = -squareSpeedY[i];
-              }
-            // if touch bottom
-            else if(squareY <= 0){
-              squareSpeedY[i] = -squareSpeedY[i];
-              }
+  
+  $("div.square").each(function(){
+    setInterval(function makeMove(square){
+      $("div.square").each(function(){
+        for(i=0; i<squareNum;i++){
+          squareY = $(this).position().top;
+          squareX = $(this).position().left;
 
-            squareX += squareSpeedX[i];
-            squareY += squareSpeedY[i];
-
-            console.log(squareX, squareY, "square " + i, "touch right ="+ (squareX >= (screenWidth - squareWidth)), "touch left = "+(squareX <= 0), squareSpeedX[i], "touch top = "+(squareY >= (screenHeight - squareHeight)), "touch bottom + "+(squareY <= 0), squareSpeedY[i]);
-
-            $(this).offset({left:squareX, top:squareY});
+          // if touch right
+          if(squareX >= (screenWidth - squareWidth) ){
+            squareSpeedX[i] = -squareSpeedX[i];
             }
-          })
-    }, 10000/framePerSecond);
-//}
+          // if touch left
+          else if(squareX <= 0){
+            squareSpeedX[i] = -squareSpeedX[i];
+            }
+          // if touch bottom
+          else if(squareY >= (screenHeight - squareHeight) ){
+            squareSpeedY[i] = -squareSpeedY[i];
+            }
+          // if touch top
+          else if(squareY <= 0){
+            squareSpeedY[i] = -squareSpeedY[i];
+            }
+
+          squareX += squareSpeedX[i];
+          squareY += squareSpeedY[i];
+
+          console.log(squareY, squareX, "square " + i, "touch right ="+ (squareX >= (screenWidth - squareWidth)), "touch left = "+(squareX <= 0), squareSpeedX[i], "touch bottom = "+(squareY >= (screenHeight - squareHeight)), "touch top + "+(squareY <= 0), squareSpeedY[i]);
+
+          $(this).offset({top:squareY, left:squareX});
+
+          console.log("square " + i, $(this).position())
+          }
+        })
+  }, 100000/framePerSecond);
+}
+)
